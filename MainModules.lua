f32b52e758d5a355bba56751fe52525399a93542
MightyyJea <111460887+MightyyJea@users.noreply.github.com>
2022-08-17 22:35:04 +0200

    Update MainModules.lua

--- MainModules.lua
+++ MainModules.lua
@@ -1,8 +1,8 @@
-local plr = game.Players
-local lplr = plr.LocalPlayer
-local char = lplr.Character
-local mouse = char:GetMouse()
-local root = char.HumanoidRootPart
-local hum = char.Humanoid
-local cam = game.Workspace.CurrentCamera
-local ReplicatedStrg = game:GetService("ReplicatedStorage")
+plr = game.Players
+lplr = plr.LocalPlayer
+char = lplr.Character
+mouse = char:GetMouse()
+root = char.HumanoidRootPart
+hum = char.Humanoid
+cam = game.Workspace.CurrentCamera
+ReplicatedStrg = game:GetService("ReplicatedStorage")
