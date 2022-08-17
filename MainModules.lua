982e31d8377f5ff88efa073674ffa5662995e21d
MightyyJea <111460887+MightyyJea@users.noreply.github.com>
2022-08-17 22:31:22 +0200

    Update MainModules.lua

--- MainModules.lua
+++ MainModules.lua
@@ -1,6 +1,6 @@
 local plr = game.Players
 local lplr = plr.LocalPlayer
-local char = lp.Character
+local char = lplr.Character
 local mouse = char:GetMouse()
 local root = char.HumanoidRootPart
 local hum = char.Humanoid
